7f2ae79f93c8235e2e7826759aef654c916f77e6
Jedi <pasit.sakorn@gmail.com>
2021-01-13 20:15:11 +0700

    Website

--- js/typeanimation.js
+++ js/typeanimation.js
@@ -2,16 +2,16 @@ var typed = new Typed(".typed", {
   strings: [
     "Join our Discord Server.",
     "Enjoy!",
-    "Come and chill.",
+    "Come and Chill.",
     "Made with ❤️ by Jedi.",
     "Have fun!",
     "Jedi is the best.",
-    "Developer.",
-    "Stay home, stay safe!",
-    "Fight COVID-19."
+    "An Official Website.",
+    "A special website for Jedi."
   ],
+ 
   typeSpeed: 80,
   loop: true,
   backSpeed: 40,
-  backDelay: 3700,
+  backDelay: 3400,
 });
